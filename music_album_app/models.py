83339148c55d32ab2924625b5d0ac0858e892e8d
coreygross <grosscor3@gmail.com>
2022-08-23 11:35:23 -0400

    homepage shows albums

---
+++ music_album_app/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Album(models.Model):
+    title = models.CharField(max_length=200)
+    artist = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True) #when item is saved, there will be a time stamp that can be used.
+
+    def __str__(self):
+        return f'{self.title} by {self.artist}'
+        
